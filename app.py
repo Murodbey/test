270effec0eeeb12af04970f942986a7430568a8e
abdullayevmurod <abdullayevmurod@gmail.com>
2025-06-11 03:48:50 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -31,6 +32,31 @@ with app.app_context():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password, method='sha256')
+
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        # We'll implement session management here later
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
