c918cd5f55e70669a6e369925e506c28ce76e28b
abdullayevmurod <abdullayevmurod@gmail.com>
2025-06-11 03:39:51 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class FamilyMember(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    photo = db.Column(db.String(200)) # Store path to photo
+    dob = db.Column(db.String(20))
+    dod = db.Column(db.String(20))
+    location = db.Column(db.String(100))
+
+    user = db.relationship('User', backref=db.backref('family_members', lazy=True))
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
