42b1d2878362fa835f56db389146c473d83eea77
abdullayevmurod <abdullayevmurod@gmail.com>
2025-06-12 03:49:27 +0000

    .

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a real secret key
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
